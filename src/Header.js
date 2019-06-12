b67e6e830f3df0c6a5820671c4e82c31b3267f1f
josecaos <jazzvoon@gmail.com>
2019-06-12 17:57:37 -0500

    Header desktop + movil

--- src/Header.js
+++ src/Header.js
@@ -1,22 +1,67 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import logo from './assets/img/codifica.logo.png';
 import './Header.css';
+
+const home_url = "localhost:3000"
 
 export class Header extends React.Component {
   render() {
     return (
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://josecaos.xyz"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <center><h1>Debug</h1></center>
+    <header className="App-header navbar navbar-expand-lg navbar-light">
+    <button class="navbar-toggler"
+    type="button"
+    data-toggle="collapse"
+    data-target="#navbar-mobile-trigger"
+    aria-controls="navbarSupportedContent"
+    aria-expanded="false"
+    aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+      <a
+      className="App-link navbar-brand"
+      href={home_url}
+      target="_blank"
+      rel="noopener noreferrer"
+      >
+      <img src={logo}
+      className="App-logo"
+      alt="logo"
+      />
+    </a>
+    <nav id="navbar-mobile-trigger" className="navbar-collapse collapse justify-content-between">
+      <Menu />
+    </nav>
+  </header>
+  );
+}
+}
+
+class Menu extends React.Component {
+  render() {
+    return (
+    <ul className="navbar-nav ml-auto">
+      <li className="nav-item">
+        <a href="#">
+          MenuItem
         </a>
-      </header>
-    )
+      </li>
+      <li className="nav-item">
+        <a href="#">
+          MenuItem
+        </a>
+      </li>
+      <li className="nav-item">
+        <a href="#">
+          MenuItem
+        </a>
+      </li>
+      <li className="nav-item">
+        <a href="#">
+          MenuItem
+        </a>
+      </li>
+    </ul>
+    );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
+// 
+import 'bootstrap/dist/js/bootstrap.js';
+//
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
