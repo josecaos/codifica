1a333b446b6750876f6fdcd09aeed05aebe02880
josecaos <jazzvoon@gmail.com>
2019-06-24 13:47:53 -0500

    Modulo navegacion + Menu

--- src/Header.js
+++ src/Header.js
@@ -1,69 +1,60 @@
-import React from 'react';
-// import ReactDOM from 'react-dom';
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import logo from './assets/img/codifica.logo.dark.png';
 import './Header.css';
 
 const home_url = "localhost:3000"
 
-export class Header extends React.Component {
-
-
+export class Header extends Component {
 
   render() {//metodo
     return (
     <header className="App-header navbar navbar-expand-lg navbar-dark">
-    <button className="navbar-toggler"
-    type="button"
-    data-toggle="collapse"
-    data-target="#navbar-mobile-trigger"
-    aria-controls="navbarSupportedContent"
-    aria-expanded="false"
-    aria-label="Toggle navigation">
+      <button className="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbar-mobile-trigger"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-      <a
-      className="App-link navbar-brand"
-      href={home_url}
-      rel="noopener noreferrer"
-      >
-      <img src={logo}
-      className="App-logo"
-      alt="logo"
-      />
-    </a>
-    <nav id="navbar-mobile-trigger" className="navbar-collapse collapse justify-content-between">
-      <Menu />
-    </nav>
-  </header>
-  );
+    <a
+    className="App-link navbar-brand"
+    href={home_url}
+    rel="noopener noreferrer"
+    >
+    <img src={logo}
+    className="App-logo"
+    alt="logo"
+    />
+  </a>
+  <nav id="navbar-mobile-trigger" className="navbar-collapse collapse justify-content-between">
+    <Menu />
+  </nav>
+</header>
+);
 }
 }
 
-class Menu extends React.Component {
+class Menu extends Component {
   render() {
     return (
+      <Router>
     <ul className="navbar-nav ml-auto">
       <li className="nav-item">
-        <a href="https://">
-          MenuItem
-        </a>
+        <Link to="/">Inicio</Link>
       </li>
       <li className="nav-item">
-        <a href="https://">
-          MenuItem
-        </a>
-      </li>
-      <li className="nav-item">
-        <a href="https://">
-          MenuItem
-        </a>
-      </li>
-      <li className="nav-item">
-        <a href="https://">
-          MenuItem
-        </a>
+        <Link to="/contacto/">Contacto</Link>
       </li>
     </ul>
+    <Route path="/contacto/" exact component={Contacto} />
+        </Router>
     );
   }
+}
+
+function Contacto() {
+    return <h2>Contacto</h2>
 }
